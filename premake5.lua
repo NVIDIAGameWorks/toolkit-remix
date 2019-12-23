77a6fd47f8381329a1102a0d439b7f994310aa1f
Anton <novoselov.ab@gmail.com>
2019-12-23 13:52:35 -0800

    premake cleanup

--- premake5.lua
+++ premake5.lua
@@ -1,58 +1,120 @@
+-- Add new option to enable passing host platform for cross-compilation
 newoption {
     trigger     = "platform-host",
     description = "(Optional) Specify host platform for cross-compilation"
 }
 
--- remove /JMC parameter for visual studio
-require('vstudio')
-premake.override(premake.vstudio.vc2010.elements, "clCompile", function(oldfn, cfg)
-    local calls = oldfn(cfg)
-    table.insert(calls, function(cfg)
-        premake.vstudio.vc2010.element("SupportJustMyCode", nil, "false")
-    end)
-    return calls
-end)
+-- Shared build scripts from repo_build package
+local repo_build = require("omni/repo/build")
 
--- shared build scripts from repo_build package
-require("omni/repo/build")
+-- Enable /sourcelink flag for VS
+repo_build.enable_vstudio_sourcelink()
 
--- local currentAbsPath = get_abs_path(".");
+-- Remove /JMC parameter for visual studio
+repo_build.remove_vstudio_jmc()
 
--- premake5.lua
+-- Setup all msvc and winsdk paths. That later can be moved to actual msvc and sdk packages 
+function setup_msvc_toolchain()
+        systemversion "10.0.17763.0"
+
+        local host_deps = "_build/host-deps"
+
+        -- system include dirs:
+        local msvcInclude = host_deps.."/msvc/VC/Tools/MSVC/14.16.27023/include"
+        local sdkInclude = { 
+            host_deps.."/winsdk/include/winrt", 
+            host_deps.."/winsdk/include/um", 
+            host_deps.."/winsdk/include/ucrt", 
+            host_deps.."/winsdk/include/shared" 
+        }
+        sysincludedirs { msvcInclude, sdkInclude }
+
+        -- system lib dirs:
+        local msvcLibs = host_deps.."/msvc/VC/Tools/MSVC/14.16.27023/lib/onecore/x64"
+        local sdkLibs = { host_deps.."/winsdk/lib/ucrt/x64", host_deps.."/winsdk/lib/um/x64" }
+        syslibdirs { msvcLibs, sdkLibs }
+
+        -- system binary dirs:
+        bindirs { 
+            host_deps.."/msvc/VC/Tools/MSVC/14.16.27023/bin/HostX64/x64", 
+            host_deps.."/msvc/MSBuild/15.0/bin", host_deps.."/winsdk/bin/x64" 
+        }
+end
+
+-- Add folder with source files to solution under impl/ virtual group (helper)
+function add_impl_folder(path)
+    files { path.."/**" }
+    vpaths {
+        ["impl/*"] = path.."/*"
+    }
+end
+
+-- Add folder with interface headers to solution under iface/ virtual group (helper)
+function add_iface_folder(path)
+    files { path.."/**" }
+    vpaths {
+        ["iface/*"] = path.."/*"
+    }
+end
+
+-- Folder to store solution in. _ACTION is compilation target, e.g.: vs2017, make etc.
+local workspace_dir = "_compiler/".._ACTION
+
+-- Target platform name, e.g. windows-x86_64
+local platform = "%{cfg.system}-%{cfg.platform}"
+
+-- Target directory
+local target_dir = "_build/"..platform.."/%{cfg.buildcfg}"
+
+-- Common plugins settings
+function define_plugin()
+    kind "SharedLib"
+    location (workspace_dir.."/%{prj.name}")
+    filter {}
+end
+
+
+-- Common python bindings settings
+function define_bindings_python(name)
+    local kit_sdk_target_deps = "_build/target-deps/kit_sdk/_build/target-deps"
+    local python_folder = kit_sdk_target_deps.."/python"
+
+    -- Carbonite carb lib
+    libdirs { kit_sdk_target_deps.."/carb_sdk_plugins/"..target_dir }
+    links {"carb" }
+
+    location (workspace_dir.."/%{prj.name}")
+
+    repo_build.define_bindings_python(name, python_folder)
+end
+
+
+-- Starting from here we define a structure of actual solution to be generated. Starting with solution name.
 workspace "kit-examples"
     configurations { "debug", "release" }
+
+    -- Project selected by default to run
     startproject ""
 
-    local targetName = _ACTION
-    local workspaceDir = "_compiler/"..targetName
+    -- Set location for solution file
+    location (workspace_dir)
 
-    -- common dir name to store platform specific files
-    local platform = "%{cfg.system}-%{cfg.platform}"
+    -- Set default target dir, later projects overwrite it
+    targetdir (target_dir)
 
-    local targetDependencyPlatform = "%{cfg.system}-%{cfg.platform}";
-    local hostDependencyPlatform = _OPTIONS["platform-host"] or targetDependencyPlatform;
-
-    local targetDir = "_build/"..platform.."/%{cfg.buildcfg}"
-
-    local hostDepsDir = "_build/host-deps"
-    local targetDepsDir = "_build/target-deps"
-    local kitSdkDir = targetDepsDir.."/kit_sdk"
-    local kitSdkTargetDepsDir = kitSdkDir.."/"..targetDepsDir
+    -- Setup include paths. Add kit SDK include paths too.
+    includedirs { 
+            "include", 
+            "_build/target-deps", 
+            "_build/target-deps/kit_sdk/include",
+            "_build/target-deps/kit_sdk/_build/target-deps/carb_sdk_plugins/include",
+            "_build/target-deps/kit_sdk/_build/target-deps/",
+    }
     
-    local carbSDKPath = kitSdkTargetDepsDir.."/carb_sdk_plugins"
-    local carbSDKInclude = carbSDKPath.."/include"
-    local carbSDKLibs = carbSDKPath.."/"..targetDir
-    
-
-    -- defining anything related to the VS or SDK version here because they will most likely be changed in the future..
-    local msvcInclude = hostDepsDir.."/msvc/VC/Tools/MSVC/14.16.27023/include"
-    local msvcLibs = hostDepsDir.."/msvc/VC/Tools/MSVC/14.16.27023/lib/onecore/x64"
-    local sdkInclude = { hostDepsDir.."/winsdk/include/winrt", hostDepsDir.."/winsdk/include/um", hostDepsDir.."/winsdk/include/ucrt", hostDepsDir.."/winsdk/include/shared" }
-    local sdkLibs = { hostDepsDir.."/winsdk/lib/ucrt/x64", hostDepsDir.."/winsdk/lib/um/x64" }
-
-    location (workspaceDir)
-    targetdir (targetDir)
+    -- Location for intermediate  files
     objdir ("_build/intermediate/"..platform.."/%{prj.name}")
+
+    -- Default compilation settings
     symbols "On"
     exceptionhandling "Off"
     rtti "Off"
@@ -60,167 +122,51 @@ workspace "kit-examples"
     flags { "FatalCompileWarnings", "MultiProcessorCompile", "NoPCH", "UndefinedIdentifiers", "NoIncrementalLink" }
     cppdialect "C++14"
 
-    includedirs { targetDepsDir, hostDepsDir, "include", 
-        kitSdkDir.."/include",
-        kitSdkDir.."/_build/target-deps/carb_sdk_plugins/include",
-        kitSdkDir.."/_build/target-deps/",
-    }
-
-    --define_buildinfo()
-
+    -- Windows platform settings
     filter { "system:windows" }
         platforms { "x86_64" }
-        -- add .editorconfig to all projects so that VS 2017 automatically picks it up
+
+        -- Add .editorconfig to all projects so that VS 2017 automatically picks it up
         files {".editorconfig"}
         editandcontinue "Off"
-        bindirs { hostDepsDir.."/msvc/VC/Tools/MSVC/14.16.27023/bin/HostX64/x64", hostDepsDir.."/msvc/MSBuild/15.0/bin", hostDepsDir.."/winsdk/bin/x64" }
-        systemversion "10.0.17763.0"
-        -- this is for the include and libs from the SDK.
-        syslibdirs { msvcLibs, sdkLibs }
-        sysincludedirs { msvcInclude, sdkInclude }
-        -- all of our source strings and executable strings are utf8
+
+        -- Enable usage of brought up toolchain
+        setup_msvc_toolchain()
+
+        -- All of our source strings and executable strings are utf8
         buildoptions {"/utf-8", "/bigobj"}
         buildoptions {"/permissive-"}
 
+
+    -- Linux platform settings
     filter { "system:linux" }
         platforms { "x86_64", "aarch64" }
         defaultplatform "x86_64"
+
         buildoptions { "-fvisibility=hidden -D_FILE_OFFSET_BITS=64" }
-        -- add library origin directory to dlopen() search path
+
+        -- Add library origin directory to dlopen() search path
         linkoptions { "-Wl,-rpath,'$$ORIGIN' -Wl,--export-dynamic" }
+        
         enablewarnings { "all" }
-        disablewarnings {
-            "undef",
-            "unused-function", -- FrameworkImpl.cpp: makeNormalizedPath, stringToLevel
-            "error=unused-variable" -- target-deps/gli: core/load_dds.inl
-        }
 
     filter { "platforms:x86_64" }
         architecture "x86_64"
 
+    -- Debug configuration settings
     filter { "configurations:debug" }
-        defines { "DEBUG", "CARB_DEBUG=1" }
+        defines { "DEBUG" }
         optimize "Off"
 
-    filter  { "configurations:release", "system:windows" }
+    -- Release configuration settings
+    filter  { "configurations:release" }
         defines { "NDEBUG" }
         optimize "Speed"
 
-    -- Linux/GCC has some issues on thread exit when the "Speed" optimizations are enabled.
-    -- We'll leave those off on Linux for the moment.
-    filter { "configurations:release", "system:linux" }
-        defines { "NDEBUG" }
-        optimize "On"
-
     filter {}
 
-    -- common plugins settings (this function is called in every plugin project
-    function define_plugin(args)
-        kind "SharedLib"
-        dependson { "carb" }
-        location (workspaceDir.."/%{prj.name}")
-        if type(args.ifaces) == "string" and type(args.impl) == "string" then
-            filesTable = {}
-            vpathsTable = {}
 
-            -- split up the project filters using the named subdirectories.
-            if type(args.subImpl) == "string" then
-                -- add all files directly in the 'impl' directory to the top level filter.
-                table.insert(filesTable, args.impl.."/*.*")
-                vpathsTable["impl"] = args.impl.."/*.*"
-
-                -- go through each named subdirectory and add all of its direct files to the
-                -- project filter of the same name.
-                impls_str = args.subImpl..";"
-                impls = split(impls_str, ";")
-
-                for idx, subDir in ipairs(impls) do
-                    table.insert(filesTable, args.impl.."/"..subDir.."/*.*")
-                    vpathsTable["impl/"..subDir] = args.impl.."/"..subDir.."/*.*"
-                end
-
-            -- recursively include all files in the 'impl' directory in one flat project filter.
-            else
-                table.insert(filesTable, args.impl.."/**.*")
-                vpathsTable["impl"] = args.impl.."/**.*"
-            end
-
-            ifaces_str = args.ifaces .. ";"
-            ifaces = split(ifaces_str, ";")
-            for idx, ifacePath in ipairs(ifaces) do
-                interfaceHeader = string.find(ifacePath, "%.h$")
-                if interfaceHeader == nil then
-                    -- Interface folder specified
-                    table.insert(filesTable, ifacePath.."/*.h")
-                    lastSlash = string.find(ifacePath, "/[^/]*$")
-                    if lastSlash == nil then
-                        vpathsTable[ifacePath] = ifacePath.."/*.h"
-                    else
-                        vpathsTable[string.sub(ifacePath, lastSlash + 1)] = ifacePath.."/*.h"
-                    end
-                else
-                    -- Interface header specified
-                    table.insert(filesTable, ifacePath)
-                    lastSlash = string.find(ifacePath, "/[^/]*$")
-                    ifacePathOnly = string.sub(ifacePath, 0, lastSlash - 1)
-                    lastSlash = string.find(ifacePathOnly, "/[^/]*$")
-                    if lastSlash == nil then
-                        vpathsTable[ifacePath] = ifacePath
-                    else
-                        currentIfacePath = string.sub(ifacePathOnly, lastSlash + 1)
-                        vpathsTable[currentIfacePath] = vpathsTable[currentIfacePath] or {}
-                        table.insert(vpathsTable[currentIfacePath], ifacePath)
-                    end
-                end
-            end
-
-            files { filesTable }
-            vpaths { vpathsTable }
-        end
-        filter {}
-    end
-
-    function define_bindings_python(args)
-        local name = args.name
-        local namespace = "carb"
-        if type(args.namespace) == "string" then
-            namespace = args.namespace
-        end
-        local folder = "source/bindings/python/"..namespace.."."..name
-        if type(args.folder) == "string" then
-            folder = args.folder
-        end
-        kind "SharedLib"
-        targetname(name)
-        targetdir (targetDir.."/bindings-python/"..namespace)
-        files { folder.."/*.*", "source/bindings/python/*.*" }
-        vpaths { [''] = folder.."/*.*", ['common'] = "source/bindings/python/*.*" }
-        dependson { "carb" }
-        links {"carb" }
-        libdirs { carbSDKLibs }
-        location (workspaceDir.."/%{prj.name}")
-        exceptionhandling "On"
-        rtti "On"
-        -- Python bindings use dynamic runtime because of pybind internal design. It is important to use the same runtime
-        -- for all bindings. We use release/debug runtime depending on configuration, which is default behavior.
-        -- carb.scrpting-python.plugin should do the same.
-        staticruntime "Off"
-        filter { "configurations:debug" }
-            runtime "Debug"
-        filter { "system:windows" }
-            targetextension(".pyd")
-            libdirs { kitSdkTargetDepsDir.."/python/libs" }
-            includedirs { kitSdkTargetDepsDir.."/python/include" }
-        filter { "system:linux" }
-            targetprefix("")
-            includedirs { kitSdkTargetDepsDir.."/python/include/python3.6m" }
-            links { kitSdkTargetDepsDir.."/python/lib/python3.6m" }
-            -- show undefined symbols as linker errors
-            linkoptions { "-Wl,--no-undefined" }
-        filter {}
-    end
-
-group "experiences"
+group "apps"
     project "examples.only"
         kind "MakeFile"
         debugcommand ("_build/target-deps/kit_sdk/_build/"..platform.."/%{cfg.buildcfg}/omniverse-kit.exe" )
@@ -228,28 +174,29 @@ group "experiences"
 group "example.python_extension"
     project "example.python_extension"
         kind "None"
-        files { "source/extensions/example.python_extension/**.py" }
+        add_impl_folder("source/extensions/example.python_extension")
 
 group "example.cpp_extension"
     project "example.cpp_extension.plugin"
-            define_plugin { ifaces = "", impl = "source/extensions/example.cpp_extension/plugins" }
-            targetdir (targetDir.."/extensions/omni/example/cpp_extension/bin/"..platform.."/%{cfg.buildcfg}")
-            location (workspaceDir.."/%{prj.name}")
+        define_plugin()
+        add_impl_folder ("source/extensions/example.cpp_extension/plugins")
+        targetdir (target_dir.."/extensions/omni/example/cpp_extension/bin/"..platform.."/%{cfg.buildcfg}")
+        location (workspace_dir.."/%{prj.name}")
 
 group "example.mixed_extension"
     project "example.mixed_extension"
         kind "None"
-        files { "source/extensions/example.mixed_extension/python/**.py" }
+        add_impl_folder("source/extensions/example.mixed_extension/python")
 
     project "example.battle_simulator.plugin"
-            define_plugin { ifaces = "include/omni/example", impl = "source/extensions/example.mixed_extension/plugins" }
-            targetdir (targetDir.."/extensions/omni/example/mixed_extension/bin/"..platform.."/%{cfg.buildcfg}")
-            location (workspaceDir.."/%{prj.name}")
+        define_plugin()
+        add_impl_folder("source/extensions/example.mixed_extension/plugins")
+        add_iface_folder("include/omni/example")
+        targetdir (target_dir.."/extensions/omni/example/mixed_extension/bin/"..platform.."/%{cfg.buildcfg}")
+        location (workspace_dir.."/%{prj.name}")
 
     project "example.battle_simulator.python"
-            define_bindings_python {
-                name = "_battle_simulator",
-                folder = "source/extensions/example.mixed_extension/bindings",
-                namespace = "omni" }
-            targetdir (targetDir.."/extensions/omni/example/mixed_extension/bindings")
+        define_bindings_python("_battle_simulator")
+        add_impl_folder("source/extensions/example.mixed_extension/bindings")
+        targetdir (target_dir.."/extensions/omni/example/mixed_extension/bindings")
         
