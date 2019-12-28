1bf4b25177986108c119c2a0513830dd9541fecb
Anton Novoselov <anovoselov@nvidia.com>
2019-12-27 18:15:16 -0800

    fix step

--- tools/ci/windows-x86_64/step.bat
+++ tools/ci/windows-x86_64/step.bat
@@ -10,8 +10,8 @@ if %errorlevel% neq 0 ( exit /b %errorlevel% )
 
 :: Docs
 :: ENABLE after fixing Kit package
-::call "%~dp0..\..\build_docs.bat" -c release
-::if %errorlevel% neq 0 ( exit /b %errorlevel% )
+call "%~dp0..\..\build_docs.bat" -c release
+if %errorlevel% neq 0 ( exit /b %errorlevel% )
 
 :: Run python tests
 call "%~dp0..\..\test_runner.bat" --suite pythontests --config debug
@@ -23,7 +23,7 @@ if %errorlevel% neq 0 ( exit /b %errorlevel% )
 ::if %errorlevel% neq 0 ( exit /b %errorlevel% )
 
 :: Package
-call "%~dp0..\package.bat"
+call "%~dp0..\..\package.bat"
 if %errorlevel% neq 0 ( exit /b %errorlevel% )
 
 :: publish artifacts to teamcity
