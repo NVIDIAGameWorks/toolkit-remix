c7a2ac2143362a3d65f8b2a416ac5b224211175f
Anton <novoselov.ab@gmail.com>
2020-09-15 20:39:40 -0700

    update repoman

--- tools/repoman/repoman.py
+++ tools/repoman/repoman.py
@@ -1,6 +1,7 @@
 import os
 import sys
-import logging
+import io
+import contextlib
 import packmanapi
 
 REPO_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../..")
@@ -11,9 +12,10 @@ def bootstrap():
     """
     Bootstrap all omni.repo modules.
 
-    Pull with packman from repo-deps.packman.xml and add them all to python sys.path to enable auto discovery by repoman.
+    Pull with packman from repo.packman.xml and add them all to python sys.path to enable importing.
     """
-    deps = packmanapi.pull(REPO_DEPS_FILE)
+    with contextlib.redirect_stdout(io.StringIO()):
+        deps = packmanapi.pull(REPO_DEPS_FILE)
     for dep_path in deps.values():
         if dep_path not in sys.path:
             sys.path.append(dep_path)
