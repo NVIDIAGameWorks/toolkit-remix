e6681b128509d1cfe9db5ebc08d4baa91f504978
Anton <novoselov.ab@gmail.com>
2021-01-11 17:34:29 -0800

    update kit and all repo tools

--- source/extensions/example.python_ext/premake5.lua
+++ source/extensions/example.python_ext/premake5.lua
@@ -3,3 +3,8 @@ local ext = get_current_extension_info()
 
 -- That will also link whole current "target" folder into as extension target folder:
 project_ext (ext)
+    repo_build.prebuild_link {
+        { "data", ext.target_dir.."/data" },
+        { "docs", ext.target_dir.."/docs" },
+        { "example", ext.target_dir.."/example" },
+    }
