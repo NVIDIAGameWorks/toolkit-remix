6f06269f7b593258f6a4afc29a3207f5fd3d2399
Anton <anovoselov@nvidia.com>
2021-03-23 18:08:41 -0700

    fix test CI

--- tools/ci/testing/test-linux-x86_64/step.sh
+++ tools/ci/testing/test-linux-x86_64/step.sh
@@ -4,8 +4,5 @@ set -e
 
 SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
 
-# pull kit sdk
-"$SCRIPT_DIR/../../../../tools/pull_kit_sdk.sh" -c release
-
 # tests
 "$SCRIPT_DIR/../../../../repo.sh" test --config release
--- tools/ci/testing/test-windows-x86_64/step.bat
+++ tools/ci/testing/test-windows-x86_64/step.bat
@@ -1,9 +1,5 @@
 @echo off
 
-:: pull kit
-call "%~dp0..\..\..\..\tools\pull_kit_sdk.bat" -c release
-if %errorlevel% neq 0 ( exit /b %errorlevel% )
-
 :: tests
 call "%~dp0..\..\..\..\repo.bat" test --config release
 if %errorlevel% neq 0 ( exit /b %errorlevel% )
