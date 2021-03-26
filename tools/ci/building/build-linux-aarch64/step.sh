09b4b18ce21bb80bf84ed678627346d9d909137a
Anton <anovoselov@nvidia.com>
2021-03-25 19:00:52 -0700

    pass platform for --platform-target linux-aarch64

--- tools/ci/building/build-linux-aarch64/step.sh
+++ tools/ci/building/build-linux-aarch64/step.sh
@@ -8,13 +8,13 @@ SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
 # "$SCRIPT_DIR/../../../../format_code.sh" --verify
 
 # Full rebuild
-"$SCRIPT_DIR/../../../../build.sh" -x
+"$SCRIPT_DIR/../../../../build.sh" -x --platform-target linux-aarch64
 
 # Package all
-"$SCRIPT_DIR/../../../../tools/package.sh" -a -c release
+"$SCRIPT_DIR/../../../../tools/package.sh" -a -c release --platform-target linux-aarch64
 
 # Package all
-"$SCRIPT_DIR/../../../../tools/package.sh" -a -c debug
+"$SCRIPT_DIR/../../../../tools/package.sh" -a -c debug --platform-target linux-aarch64
 
 # publish artifacts to teamcity
 echo "##teamcity[publishArtifacts '_build/packages']"
