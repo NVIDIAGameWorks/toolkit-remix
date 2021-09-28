8a65ac316f829e042a9fd688b9bdbee72a4c1e31
adunn <adunn@nvidia.com>
2021-09-28 15:17:55 -0700

    Adding dummy lightspeed exporter extension

---
+++ source/extensions/lightspeed.export/lightspeed/export/exporter.py
@@ -0,0 +1,18 @@
+import omni.ext
+
+def some_public_function(x: int):
+    print(f"[example.python_ext] some_public_function was called with {x}")
+    return x ** x
+
+
+# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
+# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
+# on_shutdown() is called.
+class LightspeedExporterExtension(omni.ext.IExt):
+    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
+    # this extension is located on filesystem.
+    def on_startup(self, ext_id):
+        print("[lightspeed.export] LightspeedExporterExtension startup")
+
+    def on_shutdown(self):
+        print("[lightspeed.export] LightspeedExporterExtension shutdown")
