cd71fb8a6005c792506db9adc22915e328a38a2b
Anton <anovoselov@nvidia.com>
2021-12-14 17:59:38 -0800

    fix launcher package, lock deps

--- premake5.lua
+++ premake5.lua
@@ -7,4 +7,9 @@ root = repo_build.get_abs_path(".")
 -- Insert kit template premake configuration, it creates solution, finds extensions.. Look inside for more details.
 dofile("_repo/deps/repo_kit_tools/kit-template/premake5.lua")
 
+repo_build.prebuild_copy {
+    { "launcher.toml", bin_dir },
+    { "source/shell/*${shell_ext}", bin_dir },
+}
+
 define_app("omni.app.lightspeed")
