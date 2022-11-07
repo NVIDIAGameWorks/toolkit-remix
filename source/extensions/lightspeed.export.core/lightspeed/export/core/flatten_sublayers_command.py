68ee53fbf333d0b08cdb858ee1291d89c3190e83
Mark Henderson <markh@nvidia.com>
2022-11-07 13:49:38 -0800

    [TREX-2111] support multiple replacement layers in the export flow.

---
+++ source/extensions/lightspeed.export.core/lightspeed/export/core/flatten_sublayers_command.py
@@ -0,0 +1,43 @@
+"""
+* Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
+*
+* NVIDIA CORPORATION and its licensors retain all intellectual property
+* and proprietary rights in and to this software, related documentation
+* and any modifications thereto.  Any use, reproduction, disclosure or
+* distribution of this software and related documentation without an express
+* license agreement from NVIDIA CORPORATION is strictly prohibited.
+"""
+from typing import Union
+
+import omni
+from omni.kit.usd.layers import FlattenLayersCommand
+from pxr import Sdf
+
+
+class FlattenSubLayersCommand(FlattenLayersCommand):
+    """Flatten Sublayers undoable **Command**."""
+
+    def __init__(self, usd_context: Union[str, omni.usd.UsdContext], layer_to_flatten: Sdf.Layer):
+        """Constructor.
+
+        Keyword Arguments:
+            usd_context (Union[str, omni.usd.UsdContext]): Usd context name or instance. It uses default context if
+                                                           it's empty.
+
+            layer_to_flatten (str): The layer that is the root of the tree oflayers which will be flattened into a
+                                    single layer.
+        """
+        self._layer_to_flatten = layer_to_flatten
+        super().__init__(usd_context)
+        self._merges = []
+
+    def _get_sublayers_from_strongest_to_weakest(self):
+        root_layer = self._layer_to_flatten
+        all_layers = []
+        current_subtree_stack = []
+        self._traverse(all_layers, None, root_layer, root_layer.identifier, current_subtree_stack)
+
+        return all_layers
+
+
+omni.kit.commands.register(FlattenSubLayersCommand)
