63515f37c491310b0033bcf6a365fc1fbaf4565e
Pierre-Olivier Trottier <ptrottier@nvidia.com>
2023-09-01 17:43:00 -0700

    REMIX-1619: Changed Edit Target Exclusion rule

--- source/extensions/lightspeed.trex.replacement.core.shared/lightspeed/trex/replacement/core/shared/tests/__init__.py
+++ source/extensions/lightspeed.trex.replacement.core.shared/lightspeed/trex/replacement/core/shared/tests/__init__.py
@@ -1,1 +1,11 @@
-from .test_setup import TestSetup
+"""
+* Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
+*
+* NVIDIA CORPORATION and its licensors retain all intellectual property
+* and proprietary rights in and to this software, related documentation
+* and any modifications thereto.  Any use, reproduction, disclosure or
+* distribution of this software and related documentation without an express
+* license agreement from NVIDIA CORPORATION is strictly prohibited.
+"""
+from .unit.test_layers import *
+from .unit.test_setup import *
