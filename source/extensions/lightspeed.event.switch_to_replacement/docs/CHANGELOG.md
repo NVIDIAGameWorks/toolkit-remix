f039c6629dd8b7a1eeed487f515f047f143efa4a
Pierre-Olivier Trottier <ptrottier@nvidia.com>
2024-02-02 19:11:24 -0800

    REMIX-119: Fix + Include in app Switch To Replacement event

--- source/extensions/lightspeed.event.switch_to_replacement/docs/CHANGELOG.md
+++ source/extensions/lightspeed.event.switch_to_replacement/docs/CHANGELOG.md
@@ -1,6 +1,11 @@
 ﻿# Changelog
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [1.1.0] - 2024-02-02
+### Changed
+- Code cleanup
+- Add the ability to grab a non-default context name via settings
+
 ## [1.0.0] - 2023-10-10
 ### Changed
 - Code cleanup
--- source/extensions/lightspeed.event.switch_to_replacement/lightspeed/event/switch_to_replacement/core.py
+++ source/extensions/lightspeed.event.switch_to_replacement/lightspeed/event/switch_to_replacement/core.py
@@ -17,75 +17,94 @@ from omni.flux.utils.common import reset_default_attrs as _reset_default_attrs
 from omni.kit.usd.layers import LayerEventType, get_layer_event_payload, get_layers
 from pxr import Sdf
 
+_CONTEXT = "/exts/lightspeed.event.switch_to_replacement/context"
+
 
 class SwitchToReplacementCore(_ILSSEvent):
     def __init__(self):
         super().__init__()
         self.default_attr = {
-            "_subscription": None,
+            "_context": None,
             "_layer_manager": None,
-            "_layer_sub": None,
             "_notification_manager": None,
+            "_current_notification": None,
+            "_stage_event_subscription": None,
+            "_layer_event_subscription": None,
         }
         for attr, value in self.default_attr.items():
             setattr(self, attr, value)
-        self._context = omni.usd.get_context()
-        self._layer_manager = LayerManagerCore()
+
+        context_name = carb.settings.get_settings().get(_CONTEXT) or ""
+        self._context = omni.usd.get_context(context_name)
+        self._layer_manager = LayerManagerCore(context_name)
+
         self._notification_manager = nm.manager.NotificationManager()
         self._notification_manager.on_startup()
-        self.__current_notification = None
+        self._current_notification = None
 
     @property
     def name(self) -> str:
         """Name of the event"""
-        return "Switch to Replacement"
+        return "SwitchToReplacement"
 
     def _install(self):
         """Function that will create the behavior"""
-        self._subscription = self._context.get_stage_event_stream().create_subscription_to_pop(
-            self.__on_save_open_event, name="Recent file saved"
-        )
         layers = get_layers()
         event_stream = layers.get_event_stream()
-        self._layer_sub = event_stream.create_subscription_to_pop(self._on_layer_event, name="layer events")
-
-    def _on_layer_event(self, event: carb.events.IEvent):
-        temp = get_layer_event_payload(event)
-        if temp.event_type in [
-            LayerEventType.EDIT_TARGET_CHANGED,
-            LayerEventType.SUBLAYERS_CHANGED,
-            LayerEventType.LOCK_STATE_CHANGED,
-            LayerEventType.EDIT_MODE_CHANGED,
-        ]:
-            self.check_current_edit_layer()
+        self._stage_event_subscription = self._context.get_stage_event_stream().create_subscription_to_pop(
+            self.__on_stage_event, name="StageEventSubscription"
+        )
+        self._layer_event_subscription = event_stream.create_subscription_to_pop(
+            self.__on_layer_event, name="LayerEventSubscription"
+        )
 
     def _uninstall(self):
         """Function that will delete the behavior"""
-        self._subscription = None
-        self._layer_sub = None
+        self._stage_event_subscription = None
+        self._layer_event_subscription = None
 
-    def __on_save_open_event(self, event):
-        if event.type in [int(omni.usd.StageEventType.SAVED), int(omni.usd.StageEventType.OPENED)]:
-            self.check_current_edit_layer()
+    def __on_layer_event(self, event: carb.events.IEvent):
+        payload = get_layer_event_payload(event)
+        if payload.event_type in [
+            LayerEventType.EDIT_MODE_CHANGED,
+            LayerEventType.EDIT_TARGET_CHANGED,
+            LayerEventType.LOCK_STATE_CHANGED,
+            LayerEventType.MUTENESS_SCOPE_CHANGED,
+            LayerEventType.MUTENESS_STATE_CHANGED,
+            LayerEventType.SUBLAYERS_CHANGED,
+        ]:
+            self.__check_current_edit_layer()
 
-    def _show_message(self, message):
-        if self.__current_notification:
-            self._notification_manager.remove_notification(self.__current_notification)
-            self.__current_notification.dismiss()
+    def __on_stage_event(self, event):
+        if event.type in [int(omni.usd.StageEventType.SAVING), int(omni.usd.StageEventType.OPENED)]:
+            self.__check_current_edit_layer()
+
+    def __show_message(self, message):
+        if self._current_notification:
+            self._notification_manager.remove_notification(self._current_notification)
+            self._current_notification.dismiss()
         ok_button = nm.NotificationButtonInfo("OK")
         ni = nm.notification_info.NotificationInfo(message, False, 3, nm.NotificationStatus.WARNING, [ok_button])
-        self.__current_notification = self._notification_manager.post_notification(ni)
+        self._current_notification = self._notification_manager.post_notification(ni)
         carb.log_warn(message)
 
-    def check_current_edit_layer(self):
-        def get_sublayer(layer):
-            result = []
-            for sublayer in layer.subLayerPaths:
-                sublayer_identifier = layer.ComputeAbsolutePath(sublayer)
-                result.append(sublayer_identifier)
-                sublayer = Sdf.Find(sublayer_identifier)
-                result.extend(get_sublayer(sublayer))
-            return result
+    def __check_current_edit_layer(self):
+        def get_sublayers(layer):
+            sublayers = []
+            submods = []
+            for sublayer_path in layer.subLayerPaths:
+                sublayer = Sdf.Layer.FindOrOpenRelativeToLayer(layer, sublayer_path)
+                if not sublayer:
+                    continue
+                if self._layer_manager.get_custom_data_layer_type(sublayer) == LayerType.replacement.value:
+                    submods.append(sublayer.identifier)
+                else:
+                    sublayers.append(sublayer.identifier)
+
+                child_sublayers, child_submods = get_sublayers(sublayer)
+                sublayers.extend(child_sublayers)
+                submods.extend(child_submods)
+            return sublayers, submods
 
         layer_replacement = self._layer_manager.get_layer(LayerType.replacement)
         # we only save stage that have a replacement layer
@@ -94,31 +113,30 @@ class SwitchToReplacementCore(_ILSSEvent):
             # checking is there is a capture layer. If there is one, we need a replacement layer. If not, we don't care
             layer_capture = self._layer_manager.get_layer(LayerType.capture)
             if layer_capture:
-                message = "Can't find the replacement layer in the current stage"
-                self._show_message(message)
+                self.__show_message("Can't find the mod layer in the current stage")
                 return
             return
+
+        sublayers, submods = get_sublayers(layer_replacement)
+
+        valid_layer_identifiers = [layer_replacement.identifier]
+        valid_layer_identifiers.extend(sublayers)
+
         stage = self._context.get_stage()
-        default_edit_target = stage.GetEditTarget().GetLayer()
-        valid_layer_paths = [layer_replacement.identifier]
-        valid_layer_paths.extend(get_sublayer(layer_replacement))
-        message = ""
+        stage_edit_target = stage.GetEditTarget().GetLayer().identifier
+
         # if the current edit target is not part of the valid layers, switch it to the replacement layer
-        if default_edit_target.identifier not in valid_layer_paths:
-            message += "Current edit layer is not valid. Switching to the replacement/mod layer"
+        if stage_edit_target not in valid_layer_identifiers:
+            messsage = (
+                "Cannot set a sub-mod as the edit target layer. Switching to the mod layer."
+                if stage_edit_target in submods
+                else "The current edit target layer is not valid. Switching to the mod layer."
+            )
+            self.__show_message(messsage)
             stage.SetEditTarget(layer_replacement)
 
-        # check if the replacement/mod layer is locked or not
-        layer_is_writable = omni.usd.is_layer_writable(layer_replacement.identifier)
-        layer_is_locked = omni.usd.is_layer_locked(self._context, layer_replacement.identifier)
-        if not layer_is_writable or layer_is_locked:
-            if message:
-                message += "\n"
-            message += "Current replacement/mod layer is locked/not writable"
-        if message:
-            self._show_message(message)
-
     def destroy(self):
-        self.__current_notification = None
+        self._current_notification = None
         self._notification_manager.on_shutdown()
+
         _reset_default_attrs(self)
