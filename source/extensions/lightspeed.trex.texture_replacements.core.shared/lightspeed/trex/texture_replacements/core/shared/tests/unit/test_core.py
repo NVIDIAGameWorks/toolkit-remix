0a3f0a22db72db214a56d8c17db732d5c0c4e5b8
Pierre-Olivier Trottier <ptrottier@nvidia.com>
2025-03-14 19:21:29 -0700

    Fixed `get_texture_material_inputs` API endpoint for Kit 106.5

--- source/extensions/lightspeed.trex.texture_replacements.core.shared/lightspeed/trex/texture_replacements/core/shared/tests/unit/test_core.py
+++ source/extensions/lightspeed.trex.texture_replacements.core.shared/lightspeed/trex/texture_replacements/core/shared/tests/unit/test_core.py
@@ -15,17 +15,22 @@
 * limitations under the License.
 """
 
+from unittest.mock import Mock, call, patch
+
 import omni.usd
 from lightspeed.trex.texture_replacements.core.shared import TextureReplacementsCore
-from omni.flux.utils.widget.resources import get_test_data as _get_test_data
+from omni.flux.asset_importer.core.data_models import TEXTURE_TYPE_INPUT_MAP, TextureTypes
+from omni.flux.material_api import ShaderInfoAPI
+from omni.flux.utils.widget.resources import get_test_data
 from omni.kit.test import AsyncTestCase
 from omni.kit.test_suite.helpers import open_stage
+from pxr import Sdf
 
 
 class TestTextureReplacementsCore(AsyncTestCase):
     # Before running each test
     async def setUp(self):
-        await open_stage(_get_test_data("usd/project_example/combined.usda"))
+        await open_stage(get_test_data("usd/project_example/combined.usda"))
         self.context = omni.usd.get_context()
 
     # After running each test
@@ -34,20 +39,42 @@ class TestTextureReplacementsCore(AsyncTestCase):
             await self.context.close_stage_async()
 
     async def test_get_expected_texture_material_inputs(self):
-        # Arrange
-        core = TextureReplacementsCore(self.context.get_name())
+        for test_data in [
+            (None, list(set(TEXTURE_TYPE_INPUT_MAP.values()))),
+            (TextureTypes.DIFFUSE, ["inputs:diffuse_texture"]),
+        ]:
+            texture_type, expected_attributes = test_data
+            expected_attributes.sort()
 
-        # Act
-        shader_inputs = await core.get_expected_texture_material_inputs("/RootNode/Looks/mat_BC868CE5A075ABB1/Shader")
+            with self.subTest(name=f"texture_type_{texture_type}"):
+                # Arrange
+                core = TextureReplacementsCore(self.context.get_name())
 
-        # Assert
-        expected_attributes = [
-            "diffuse_texture",
-            "metallic_texture",
-            "normalmap_texture",
-            "reflectionroughness_texture",
-        ]
-        # ex: "
-        input_names = [shader_input.split(":")[1] for shader_input in shader_inputs]
-        self.assertTrue(shader_inputs)
-        self.assertEqual(input_names, expected_attributes)
+                prim_path = "/RootNode/Looks/mat_BC868CE5A075ABB1/Shader"
+                expected_prim = self.context.get_stage().GetPrimAtPath(Sdf.Path(prim_path).GetPrimPath())
+
+                with (
+                    patch.object(ShaderInfoAPI, "__init__") as mock_shader_info_api,
+                    patch.object(ShaderInfoAPI, "get_input_properties") as mock_get_input_properties,
+                ):
+                    mock_shader_info_api.return_value = None
+
+                    input_properties = []
+                    for input_property in TEXTURE_TYPE_INPUT_MAP.values():
+                        property_mock = Mock()
+                        property_mock.GetName.return_value = input_property
+                        input_properties.append(property_mock)
+                    mock_get_input_properties.return_value = input_properties
+
+                    # Act
+                    shader_inputs = await core.get_expected_texture_material_inputs(
+                        prim_path, texture_type=texture_type
+                    )
+
+                # Assert
+                self.assertEqual(mock_shader_info_api.call_count, 1)
+                self.assertEqual(mock_shader_info_api.call_args, call(expected_prim))
+
+                input_names = sorted([shader_input.split(".")[-1] for shader_input in shader_inputs])
+                self.assertTrue(shader_inputs)
+                self.assertEqual(input_names, expected_attributes)
